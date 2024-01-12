08fe28ec29657abe768cea63914e6bf03a270610
Piyars <piyarskakadiya@gmail.com>
2024-01-12 16:40:51 +0530

    "Added buttons and modal to add logs"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/log/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/log/AddLogModal";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
@@ -14,6 +16,8 @@ const App = () => {
     <>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </>
